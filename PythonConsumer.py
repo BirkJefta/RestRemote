c2f61e6f1a540135655aed4eaaa5cb433f3a2c6c
Birk Jefta Svensson <bis001@edu.zealand.dk>
2025-03-31 12:58:19 +0200

    foreach

--- PythonConsumer.py
+++ PythonConsumer.py
@@ -1,4 +1,9 @@
 import requests
 
 response = requests.get("http://localhost:5283/api/AIModels")
+aimodels = response.json()
 print(response)
+print(aimodels)
+
+for aimodel in aimodels:
+    print(aimodel)
